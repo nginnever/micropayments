44c71d7c1395faf93773d959dd8c8c0bbffbe68b
nginnever <ginneversource@gmail.com>
2017-05-24 20:24:23 -0400

    begin structuring

---
+++ contracts/FarmerRegisteryLibrary.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.11;
+
+library FarmerRegistryLibrary {
+
+  mapping(address => uint) balances;
+  mapping (address => mapping (address => uint)) allowed;
+
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length != size + 4) {
+       throw;
+     }
+     _;
+  }
+
+  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) returns (bool success) {
+    balances[msg.sender] = safeSub(balances[msg.sender], _value);
+    balances[_to] = safeAdd(balances[_to], _value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+}
---
+++ contracts/StorjChannel.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.11;
+
+import "./FarmerRegisteryLibrary.sol";
+
+contract StorjChannel {
+
+  using FarmerRegisteryLibrary for FarmerRegisteryLibrary.Data;
+  FarmerRegisteryLibrary.Data public data;
+
+
+  function StorjChannel(address _to, uint _value) {
+
+  }
+}
